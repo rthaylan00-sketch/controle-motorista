0a5907db405a3ec5ac1a9dbb39ebd1fee2d2354d
rthaylan00-sketch <rthaylan00@gmail.com>
2026-03-11 17:37:15 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,53 @@
+import { auth } from "./firebase.js";
 
+import { 
+signInWithEmailAndPassword,
+onAuthStateChanged,
+signOut
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+// LOGIN
+window.login = async function(){
+
+const email = document.getElementById("email").value;
+const senha = document.getElementById("senha").value;
+
+try{
+
+await signInWithEmailAndPassword(auth, email, senha);
+
+window.location.href = "index.html";
+
+}catch(erro){
+
+alert("Erro no login");
+
+}
+
+}
+
+
+// VERIFICAR USUÁRIO
+onAuthStateChanged(auth,(user)=>{
+
+if(user){
+
+console.log("Usuário logado:",user.email);
+
+}else{
+
+console.log("Nenhum usuário");
+
+}
+
+});
+
+
+// LOGOUT
+window.logout = async function(){
+
+await signOut(auth);
+
+window.location.href = "login.html";
+
+}
