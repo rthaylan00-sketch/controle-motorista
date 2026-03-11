ab4c607cd21185b7d534dbe3c807fc7b92f2ab89
rthaylan00-sketch <rthaylan00@gmail.com>
2026-03-11 17:27:46 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,23 @@
+// Importações Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+// CONFIG DO SEU FIREBASE
+const firebaseConfig = {
+  apiKey: "SUA_API_KEY",
+  authDomain: "SEU_PROJETO.firebaseapp.com",
+  projectId: "SEU_PROJETO",
+  storageBucket: "SEU_PROJETO.appspot.com",
+  messagingSenderId: "XXXX",
+  appId: "XXXX"
+};
+
+// Inicializar
+const app = initializeApp(firebaseConfig);
+
+// Exportar serviços
+export const auth = getAuth(app);
+export const db = getFirestore(app);
