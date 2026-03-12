a90ec8719c64a666d1baa4ae3aa870a884db4cd1
rthaylan00-sketch <rthaylan00@gmail.com>
2026-03-12 00:19:40 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,21 @@
+<script src="https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js"></script>
 
+<script src="https://www.gstatic.com/firebasejs/10.12.2/firebase-auth-compat.js"></script>
+
+<script src="firebase.js"></script>
+<script src="app.js"></script>
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA829hZRNAJ_2xXYJkWW4skWiZLs6Zyc38",
+  authDomain: "drivecontrol-pro.firebaseapp.com",
+  projectId: "drivecontrol-pro",
+  storageBucket: "drivecontrol-pro.firebasestorage.app",
+  messagingSenderId: "367865364380",
+  appId: "1:367865364380:web:d3a69977e438ca76861259"
+};
+
+// Inicializar Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Ativar autenticação
+const auth = firebase.auth();
