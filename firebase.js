fcd8bf36a4da90f17c05a0d6cb358608b4b380bc
rthaylan00-sketch <rthaylan00@gmail.com>
2026-03-11 22:35:39 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -5,19 +5,18 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-aut
 
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-// CONFIG DO SEU FIREBASE
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_PROJETO.firebaseapp.com",
-  projectId: "SEU_PROJETO",
-  storageBucket: "SEU_PROJETO.appspot.com",
-  messagingSenderId: "XXXX",
-  appId: "XXXX"
+apiKey:            “AIzaSyBHfI8CE9eMUQ99JM_XAjEkiuFMDoz3M8I”,
+authDomain:        “controle-pro-motorista.firebaseapp.com”,
+projectId:         “controle-pro-motorista”,
+storageBucket:     “controle-pro-motorista.firebasestorage.app”,
+messagingSenderId: “991201688036”,
+appId:             “1:991201688036:web:fac529d7c0e7cad826eff1”
 };
 
-// Inicializar
-const app = initializeApp(firebaseConfig);
+// Inicializa
+firebase.initializeApp(firebaseConfig);
 
-// Exportar serviços
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+// Instâncias globais (usadas em app.js, login.html e cadastro.html)
+const auth = firebase.auth();
+const db   = firebase.firestore();
