a4718aa27b2b3ee6e3defaa31544052a425999b5
rthaylan00-sketch <rthaylan00@gmail.com>
2026-03-12 00:30:04 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
+function login(){
 
+const email = document.getElementById("email").value
+const senha = document.getElementById("senha").value
+
+auth.signInWithEmailAndPassword(email, senha)
+
+.then(()=>{
+window.location.href = "index.html"
+})
+
+.catch((error)=>{
+document.getElementById("msg").innerText = error.message
+})
+
+}
+
+function cadastrar(){
+
+const email = document.getElementById("email").value
+const senha = document.getElementById("senha").value
+
+auth.createUserWithEmailAndPassword(email, senha)
+
+.then(()=>{
+window.location.href = "index.html"
+})
+
+.catch((error)=>{
+document.getElementById("msg").innerText = error.message
+})
+
+}
+
+function logout(){
+
+auth.signOut().then(()=>{
+window.location.href = "login.html"
+})
+
+}
