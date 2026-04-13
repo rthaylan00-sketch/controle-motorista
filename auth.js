87e3d3dac4d0cc154021d0f8f8efa127c8c3cfcc
rthaylan00-sketch <rthaylan00@gmail.com>
2026-04-13 20:16:36 -0300

    Correct typos in login functionality
    Fix typos in event listener and sign-in function.

--- auth.js
+++ auth.js
@@ -4,3 +4,21 @@ import {
   createUserWithEmailAndPassword,
   signOut
 } from "https: //www.gstatic.com/firebasejs/10.12.22/firebase-auth.js";
+
+const btnLogin = document.getElementById("btn-login");
+const btnCadastrar = document.getElementById("btn-cadastrar");
+const btnLogout = document.getElementById("btn-logout")
+
+if (btnLogin) {
+  brnLogin.addEventListener("click". () => {
+     const email = document.getElementById("email").value;
+     const senha = document.getElementById("senha").value;
+
+  singInWithEmailAndPassword(auth, email, senha)
+   .then(() => { window.location.href = "index.html"; })
+   .catch(error => { document.getElementById("msg").innerText = error.message;});
+ });
+}
+  
+  
+                        
