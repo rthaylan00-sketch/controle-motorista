9946aa95796324b8fe5d29c2093ab76c9f23864a
rthaylan00-sketch <rthaylan00@gmail.com>
2026-04-13 19:41:56 -0300

    Add Firebase authentication imports to auth.js

---
+++ auth.js
@@ -0,0 +1,6 @@
+import { auth } from "./firebase.js";
+import { 
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  signOut
+} from "https: //www.gstatic.com/firebasejs/10.12.22/firebase-auth.js";
