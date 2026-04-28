56d002c833e64aecc2b3f0eda6b14906134a74b1
rthaylan00-sketch <rthaylan00@gmail.com>
2026-04-28 19:09:47 -0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -3,34 +3,34 @@ import {
   signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
   signOut
-} from "https: //www.gstatic.com/firebasejs/10.12.22/firebase-auth.js";
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 const btnLogin = document.getElementById("btn-login");
 const btnCadastrar = document.getElementById("btn-cadastrar");
 const btnLogout = document.getElementById("btn-logout")
 
 if (btnLogin) {
-  btnLogin.addEventListener("click". () => {
+  btnLogin.addEventListener("click", () => {
      const email = document.getElementById("email").value;
      const senha = document.getElementById("senha").value;
 
-  singInWithEmailAndPassword(auth, email, senha)
+  signInWithEmailAndPassword(auth, email, senha)
    .then(() => { window.location.href = "index.html"; })
    .catch(error => { document.getElementById("msg").innerText = error.message;});
  });
 }
 if (btnCadastrar) {
-  btnCadastrar.addEventListener("click". () => {
+  btnCadastrar.addEventListener("click", () => {
      const email = document.getElementById("email").value;
      const senha = document.getElementById("senha").value;
 
-  createWithEmailAndPassword(auth, email, senha)
+  createUserWithEmailAndPassword(auth, email, senha)
    .then(() => { window.location.href = "index.html"; })
    .catch(error => { document.getElementById("msg").innerText = error.message;});
  });
 }
 if (btnLogout) {
-  btnLogout.addEventListener("click". () => {
+  btnLogout.addEventListener("click", () => {
     signOut(auth).then(() => { window.location.href = "login.html"; })
  });
 }
